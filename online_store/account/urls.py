90505e4d9357d635272edf805dbba86e04574995
simonwagner <swagner648@gmail.com>
2019-02-27 18:14:53 -0600

    Initial project skeleton creation, funcitioning user authentification

---
+++ online_store/account/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from . import views
+
+app_name = 'account'
+urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+    # path('signup', views.signup, name='signup'),
+    # path('create-account', views.create_account, name='create-account'),
+    # path('forgot-password', views.forgot_password, name='forgot-password'),
+    # path('reset-email-sent', views.reset_email_sent, name='reset-email-sent'),
+    # path('profile', views.profile, name='profile'),
+    # path('settings', views.settings, name='settings')
+]
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/simonwagner/PycharmProjects/shopping_cart_software/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
