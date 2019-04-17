d132056ff4c455f7190a968f5dd9b74238220ec3
simonwagner <swagner648@gmail.com>
2019-04-16 19:03:51 -0500

    logo

--- online_store/online_store/settings.py
+++ online_store/online_store/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static-assets/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/store')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static-assets')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- online_store/store/apps.py
+++ online_store/store/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class StoreConfig(AppConfig):
-    name = 'static'
+    name = 'static-assets'
